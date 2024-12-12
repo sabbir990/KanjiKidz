24b1cec9028f2cd82c42dbeae640598ddf71f41e
Muhammad Sabbir Khan Alif <muhammadsabbirkhanalif@gmail.com>
2024-12-12 21:15:57 +0000

    Completed the lessons page

--- src/routes/User_Routes/Lessons.jsx
+++ src/routes/User_Routes/Lessons.jsx
@@ -3,6 +3,7 @@ import Head_section from '../../components/Head_section'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import LessonCard from '../../Cards/Lesson_card'
+import { TbFidgetSpinner } from 'react-icons/tb'
 
 export default function Lessons() {
     const {data : all_lessons, isLoading} = useQuery({
@@ -13,11 +14,15 @@ export default function Lessons() {
         }
     })
 
-    console.log(all_lessons)
     return (
         <div className='font-poppins'>
             <Head_section heading={'Explore Your Learning Journey'} subheading={"Browse through all the lessons and continue building your knowledge, one step at a time."} />
             <hr />
+            {
+                isLoading && <div className='flex items-center justify-center'>
+                    <TbFidgetSpinner size={34} className='animate-spin' />
+                </div>
+            }
             <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 mt-4'>
                 {
                     all_lessons?.map((lesson, index) => {
